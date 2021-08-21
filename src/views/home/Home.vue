5fdb0d98ebf8d77f6abeb20bba43e291bfeb3ca6
HY <smtownred@naver.com>
2021-08-22 01:37:20 +0800

    首页开发

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,41 +1,18 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners="banners" />
-    <recommendview :recommends="recommends" />
-    <feature-view />
-    <tab-control class="tab-control" :titles="['流行', '新款', '精选']" />
-    <ul>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-    </ul>
+    <scroll class="content" ref="scroll" :probe-type="3" @scroll="contentScroll">
+      <home-swiper :banners="banners" />
+      <recommendview :recommends="recommends" />
+      <feature-view />
+      <tab-control
+        class="tab-control"
+        :titles="['流行', '新款', '精选']"
+        @tabClick="tabClick"
+      />
+      <goods-list :goods="showGoods" />
+    </scroll>
+    <back-top @click.native="backClick" v-show="isShow" />
   </div>
 </template>
 
@@ -46,6 +23,9 @@ import FeatureView from "./childComps/FeatureView";
 
 import NavBar from "components/common/navbar/NavBar";
 import TabControl from "components/content/tabControl/TabControl";
+import GoodsList from "components/content/goods/GoodsList";
+import Scroll from "components/common/scroll/Scroll";
+import BackTop from "components/content/backTop/BackTop";
 
 import { getHomeMultidata, getHomeGoods } from "network/home";
 
@@ -57,6 +37,9 @@ export default {
     Recommendview,
     FeatureView,
     TabControl,
+    GoodsList,
+    Scroll,
+    BackTop,
   },
   data() {
     return {
@@ -64,18 +47,46 @@ export default {
       recommends: [],
       goods: {
         pop: { page: 0, list: [] },
-        news: { page: 0, list: [] },
+        new: { page: 0, list: [] },
         sell: { page: 0, list: [] },
       },
+      currentType: "pop",
+      isShow: false,
     };
+  },
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list;
+    },
   },
   created() {
     //1.请求多个数据
     this.getHomeMultidata();
     // 2.请求商品数据
-    
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
   },
   methods: {
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentType = "pop";
+          break;
+        case 1:
+          this.currentType = "new";
+          break;
+        case 2:
+          this.currentType = "sell";
+          break;
+      }
+    },
+    contentScroll(position){
+      this.isShow = -position.y >1000
+    },
+    backClick() {
+      this.$refs.scroll.scrollTo(0, 0,500);
+    },
     getHomeMultidata() {
       getHomeMultidata()
         .then((res) => {
@@ -86,21 +97,24 @@ export default {
           console.log(err);
         });
     },
-    getHomeGoods(type){
-      const page = this.goods[type].page+1;
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
       getHomeGoods(type, page).then((res) => {
-      console.log(res);
-      this.goods[type].page +=1;
-      this.goods[type].list = res.data.goods.list;
-    });
-    }
+        // console.log(res);
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+        console.log(this.goods[type].page);
+      });
+    },
   },
 };
 </script>
 
 <style scoped>
 #home {
-  padding-top: 44px;
+  /* padding-top: 44px; */
+  height: 100vh;
+  position: relative;
 }
 
 .home-nav {
@@ -116,5 +130,21 @@ export default {
 .tab-control {
   position: sticky;
   top: 44px;
+  z-index: 9;
 }
+
+.content {
+  overflow: hidden;
+
+  position: absolute;
+  top: 44px;
+  bottom: 49px;
+  left: 0;
+  right: 0;
+}
+/* .content {
+  height: calc(100% - 93px);
+  overflow: hidden;
+  margin-top: 44px;
+} */
 </style>
